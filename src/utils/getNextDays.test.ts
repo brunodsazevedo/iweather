6bef6346c252317da9c7ced329d1b44b4d4bb18d
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-10-19 02:45:07 -0300

    feat: teste uninatio de função getNextDays

--- src/utils/getNextDays.test.ts
+++ src/utils/getNextDays.test.ts
@@ -1,4 +1,8 @@
-test("should be return the next five days", () => {
-  // Executar nosso teste...
+import { getNextDays } from './getNextDays'
 
+it("should be return the next five days", () => {
+  // Executar nosso teste...
+  const days = getNextDays()
+
+  expect(days.length).toBe(5)
 })
