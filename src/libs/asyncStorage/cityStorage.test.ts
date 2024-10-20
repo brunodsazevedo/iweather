11bebf7aa0cf147ffe3c24504e566d1dd528be1b
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-10-20 17:26:44 -0300

    feat: feito testes de adicionar, pegar e remover dados de storage

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,7 +1,36 @@
-import { getStorageCity } from "./cityStorage"
+import { CityProps } from "@services/getCityByNameService"
+import { getStorageCity, saveStorageCity, removeStorageCity } from "./cityStorage"
 
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storage", async() => {
+    const response = await getStorageCity()
+    expect(response).toBeNull()
+  })
+
+  it("should be return city storage", async() => {
+    const newCity: CityProps = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456,
+    }
+    await saveStorageCity(newCity)
+
+    const response = await getStorageCity()
+
+    expect(response).toEqual(newCity)
+  })
+
+  it("should be remove city storage", async () => {
+    await saveStorageCity({
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456,
+    })
+
+    await removeStorageCity()
+
     const response = await getStorageCity()
     expect(response).toBeNull()
   })
