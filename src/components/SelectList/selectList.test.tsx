7ad40fea8970a579dfebe9570fce9c03522ac683
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-10-20 16:15:55 -0300

    feat: feito teste para serlectList para verificar se uma cidade foi selecionada

---
+++ src/components/SelectList/selectList.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from '@testing-library/react-native'
+
+import { SelectList } from '@components/SelectList'
+
+describe('Component: SelectList', () => {
+  it('should be return city details selected', () => {
+    const data = [
+      {
+        id: '1',
+        name: 'Campinas',
+        latitude: 123,
+        longitude: 456,
+      },
+      {
+        id: '2',
+        name: 'Cascavel',
+        latitude: 987,
+        longitude: 789,
+      },
+    ]
+
+    const onPress = jest.fn()
+
+    render(
+      <SelectList
+        data={data}
+        onChange={() => {}}
+        onPress={onPress}
+      />
+    )
+
+    const selectedCity = screen.getByText(/Cascavel/i)
+    fireEvent.press(selectedCity)
+
+    expect(onPress).toHaveBeenCalledWith(data[1])
+  })
+})
