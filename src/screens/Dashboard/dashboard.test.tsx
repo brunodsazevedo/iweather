4988cc49b8dc88a693ed1eddbdc3029aa45e2ea1
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-10-30 08:27:43 -0300

    fix: correção de teste Dashboard de erro de timeout

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -23,7 +23,7 @@ describe("Screens: Dashboard", () => {
 
     render(<Dashboard />)
 
-    const cityName = await waitFor(() =>screen.findByText(/rio do sul/i) )
+    const cityName = await waitFor(() =>screen.findByText(/rio do sul/i, {}, { timeout: 3000 }) )
     expect(cityName).toBeTruthy()
   })
 
